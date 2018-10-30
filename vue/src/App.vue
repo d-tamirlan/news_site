77129c24e2a734a41f50d0122c5738778957a1cc
Tamirlan <konoor@yandex.ru>
2018-10-30 21:56:27 +0300

    add getAllCategories method and render categories in App.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,60 +1,59 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    <h1>News site</h1>
+    <ul class="categories-list">
+      <li v-for="category in categories.results" class="categories-list__category">
+        {{ category.name }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import axios from 'axios'
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        rootDomain: 'http://localhost:8000/',
+        categories: [],
+      }
+    },
+    created(){
+      this.getAllCategories();
+    },
+    methods:{
+      getAllCategories(){
+        let categories_url = this.rootDomain+'categories-list/';
+
+        axios.get(categories_url).then(response => {
+          this.categories = response.data;
+          console.log(this.categories)
+        }).catch(error => {
+          console.log('--error--');
+          console.log(error);
+        })
+      }
     }
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .categories-list{
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .categories-list__category{
+    margin-left: 20px;
+  }
 </style>
